48648ffc8cf22bf284e295857ce43d8f511620e2
nikodev13 <niko.dev@outlook.com>
2022-09-30 11:45:24 +0200

    WeatherForecast tutorial

--- src/API/ECommerce.API/Controllers/WeatherForecastController.cs
+++ src/API/ECommerce.API/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using ECommerce.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.API.Controllers;
@@ -6,27 +7,15 @@ namespace ECommerce.API.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    private readonly IWeatherForecastService _service;
+    public WeatherForecastController(IWeatherForecastService service)
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
+        _service = service;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return _service.Get();
     }
 }
---
+++ src/API/ECommerce.API/Services/WeatherForecastService.cs
@@ -0,0 +1,29 @@
+﻿namespace ECommerce.API.Services;
+
+public class WeatherForecastService : IWeatherForecastService
+{
+    private readonly ILogger<WeatherForecastService> _logger;
+
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public WeatherForecastService(ILogger<WeatherForecastService> logger)
+    {
+        _logger = logger;
+    }
+    
+    public IEnumerable<WeatherForecast> Get()
+    {
+        _logger.LogInformation("aaa");
+        
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
